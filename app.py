85952b52338cc75055e672efa2f099c8ecd4270f
themysticsavages <the.mystic.6660@gmail.com>
2023-07-18 10:20:19 -0400

    fixed links and added app handlers

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.get("/")
 def home():
     return render_template("index.html")
+
+@app.get("/about")
+def about():
+    return render_template("about.html")
+
+@app.get("/stats")
+def stats():
+    return render_template("stats.html")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html")
