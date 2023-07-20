c99ef90aaaced69bc00e03cb2ed6e0586f5df38c
mkacere <maxkacere@gmail.com>
2023-07-20 10:26:27 -0400

    Co-authored-by: themysticsavages <themysticsavages@users.noreply.github.com>

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ def about():
 def stats():
     return render_template("stats.html")
 
+@app.get("/recipeinfo")
+def recipeinfo():
+    return render_template("recipeinfo.html")
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("404.html")
