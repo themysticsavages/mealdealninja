2503cbb2012aed196400e9bd21b136340b1d2bbb
themysticsavages <the.mystic.6660@gmail.com>
2023-07-24 11:30:10 -0400

    try to incorporate data into page

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
+from functools import cache
+
 from flask import Flask, render_template
 from flask_ngrok import run_with_ngrok
 
 import pandas as pd
 
 
+@cache
+def dataframe():
+    return pd.read_csv("data/200recipes.csv", index_col="Unnamed: 0")
+
+
 app = Flask(__name__)
 run_with_ngrok(app)
 
+
 @app.get("/")
 def home():
-    return render_template("index.html", title="Home")
+    df = dataframe().sort_values(by=["rating", "Price"], ascending=[False, True])
+    df.rename(columns={"Price": "cost"}, inplace=True)
+    cards = df[["title", "cost"]].to_dict(orient="records")[:10]
+    return render_template("index.html", title="Home", cards=cards)
 
 
 @app.get("/about")
@@ -30,6 +41,3 @@ def recipeinfo():
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("404.html", title="Not found")
-
-if __name__ == "__main__":
-    app.run()
